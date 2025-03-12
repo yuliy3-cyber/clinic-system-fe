5085e29ac3a6984caa08cef81430ca74b5676a0d
yuliy3-cyber <datgosuyt@gmail.com>
2025-03-13 02:18:56 +0700

    add login function

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { AppointmentListComponent } from './pages/appointment-list/appointment-list.component';
+import { NewAppointmentComponent } from './pages/new-appointment/new-appointment.component';
+import { PatientListComponent } from './pages/patient-list/patient-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'appointment-list',
+        component: AppointmentListComponent,
+      },
+      {
+        path: 'new-appointment',
+        component: NewAppointmentComponent,
+      },
+      {
+        path: 'patient-list',
+        component: PatientListComponent,
+      },
+    ],
+  },
+];
